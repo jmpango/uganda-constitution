e776328273e40ea39ce0e3a02b13f8e66cd5d67f
jmpango <mpiksjonathan@gmail.com>
2013-01-08 17:38:13 +0300

    Mavenized project with the model, server and widget packages

---
+++ uganda-constitution/src/main/java/org/uganda/constitution/StringConstants.java
@@ -0,0 +1,10 @@
+package org.uganda.constitution;
+
+/**
+ * Handles all string constants for the application.
+ * 
+ * @author Jonathan
+ */
+public class StringConstants {
+    public static String APPLICATION_TITLE = "Constitution of the Republic of Uganda";
+}
---
+++ uganda-constitution/src/main/java/org/uganda/constitution/model/BaseData.java
@@ -0,0 +1,12 @@
+package org.uganda.constitution.model;
+
+/**
+ * Base class for all Data Objects in the application.
+ *
+ * @author Jonathan
+ */
+public class BaseData {
+
+    private String id = null;
+    private RecordStatus recordStatus = RecordStatus.ACTIVE;
+}
