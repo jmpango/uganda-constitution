b4171314127493b88cff094dc39d99851c2be3f2
jmpango <mpiksjonathan@gmail.com>
2013-01-10 18:16:20 +0300

    Create the constitution service class.

--- uganda-consititution-api/src/main/java/org/uganda/constitution/api/service/ConstitutionService.java
+++ uganda-consititution-api/src/main/java/org/uganda/constitution/api/service/ConstitutionService.java
@@ -1,14 +1,60 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.uganda.constitution.api.service;
 
+import java.util.List;
+import org.uganda.constitution.api.model.Constitution;
+import org.uganda.constitution.api.model.exception.ValidationException;
+
 /**
- *
  * @author Jonathan
  */
 public interface ConstitutionService {
+    /**
+     * Saves a constitution.
+     * @param constition
+     * @throws ValidationException
+     */
+    void save(Constitution constition) throws ValidationException;
+
+    /**
+     * Deletes a constitution.
+     * @param constition
+     * @throws ValidationException
+     */
+    void delete(Constitution constition) throws ValidationException;
+
+    /**
+     * Validates a constitution before saving.
+     * @param constitution
+     * @throws ValidationException
+     */
+    void validateConstitution(Constitution constitution) throws ValidationException;
+
+    /**
+     * Gets a constitution based on constitution id.
+     * @param id
+     * @return
+     */
+    Constitution getConstitution(String id);
+
+    /**
+     * Gets a constitution based on constitution year.
+     * @param year
+     * @return
+     */
+    Constitution getConstitutionByYear(int year);
+
+    /**
+     * Gets a constitution based on constitution language.
+     * @param language
+     * @return
+     */
+    Constitution getConstitutionByLanguage(String language);
+
+    /**
+     * Gets  a list of constitions.
+     * @return
+     */
+    List<Constitution> getConstitutions();
+    
 
 }
--- uganda-consititution-api/src/main/java/org/uganda/constitution/api/service/impl/ConstitutionServiceImpl.java
+++ uganda-consititution-api/src/main/java/org/uganda/constitution/api/service/impl/ConstitutionServiceImpl.java
@@ -1,16 +1,65 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.uganda.constitution.api.service.impl;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.uganda.constitution.api.dao.ConstitutionDAO;
+import org.uganda.constitution.api.model.Constitution;
+import org.uganda.constitution.api.model.RecordStatus;
+import org.uganda.constitution.api.model.exception.ValidationException;
 import org.uganda.constitution.api.service.ConstitutionService;
 
 /**
- *
  * @author Jonathan
  */
-public class ConstitutionServiceImpl implements ConstitutionService{
+@Service("constitutionService")
+public class ConstitutionServiceImpl implements ConstitutionService {
 
+    @Autowired
+    private ConstitutionDAO constitutionDAO;
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void save(Constitution constition) throws ValidationException {
+        constitutionDAO.save(constition);
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void delete(Constitution constition) throws ValidationException {
+        constitutionDAO.delete(constition);
+    }
+
+    @Override
+    public void validateConstitution(Constitution constitution) throws ValidationException {
+        if (constitution == null) {
+            throw new ValidationException("supplied constitution is null");
+        }
+    }
+
+    @Override
+    @Transactional(readOnly=true)
+    public Constitution getConstitution(String id) {
+        return constitutionDAO.searchUniqueByPropertyEqual("id", id);
+    }
+
+    @Override
+    @Transactional(readOnly=true)
+    public Constitution getConstitutionByYear(int year) {
+        return constitutionDAO.searchUniqueByPropertyEqual("year", year);
+    }
+
+    @Override
+    @Transactional(readOnly=true)
+    public Constitution getConstitutionByLanguage(String language) {
+        return constitutionDAO.searchUniqueByPropertyEqual("language", language);
+    }
+
+    @Override
+    @Transactional(readOnly=true)
+    public List<Constitution> getConstitutions() {
+        return constitutionDAO.searchByRecordStatus(RecordStatus.ACTIVE);
+    }
 }
